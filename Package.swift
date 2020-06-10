58d9141d566a145981802e876205fdbcece6308a
xaoxuu <xaoxuu@gmail.com>
2020-06-10 13:14:20 +0800

    发布 swift package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ProHUD",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "ProHUD", targets: ["ProHUD"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", "5.0.0" ..< "6.0.0"),
+        .package(url: "https://github.com/xaoxuu/Inspire.git", "1.1.0" ..< "2.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "ProHUD",
+            dependencies: ["SnapKit", "Inspire"],
+            path: "Source"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
